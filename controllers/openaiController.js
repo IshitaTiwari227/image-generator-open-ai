67d5104244e2c4954003c11bef8fa38a3ee795ef
IshitaTiwari227 <Ishita_tiwari@condenast.com>
2023-06-21 16:42:13 +0530

    used stable diffusion

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,24 +1,44 @@
-const { Configuration, OpenAIApi } = require('openai');
+const axios = require("axios");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const apiKey = "OzWGnCgcfwadqgnDPyhaPcSP5Rtk4hUwR9FihZbUQONhPzDCmR3kRSctIFAZ";
+const numberOfPics = "1"; //Limitation of 4 maximum image generation per call observed.
 
 const generateImage = async (req, res) => {
-  const { prompt, size } = req.body;
+  const { prompt } = req.body;
 
-  const imageSize =
-    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+  const bodyInfo = JSON.stringify({
+    key: apiKey,
+    prompt: prompt,
+    negative_prompt:
+      "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime",
+    width: "512",
+    height: "512",
+    samples: numberOfPics,
+    num_inference_steps: "30",
+    safety_checker: "no",
+    enhance_prompt: "yes",
+    seed: null,
+    guidance_scale: 7.5,
+    webhook: null,
+    track_id: null,
+  });
+
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
   try {
-    const response = await openai.createImage({
-      prompt,
-      n: 1,
-      size: imageSize,
-    });
+    console.log("Generating Images....");
+    console.log("");
+    const result = await axios.post(
+      "https://stablediffusionapi.com/api/v3/text2img",
+      bodyInfo,
+      options
+    );
 
-    const imageUrl = response.data.data[0].url;
+    const imageUrl = result.data.output[0];
 
     res.status(200).json({
       success: true,
@@ -34,7 +54,7 @@ const generateImage = async (req, res) => {
 
     res.status(400).json({
       success: false,
-      error: 'The image could not be generated',
+      error: "The image could not be generated",
     });
   }
 };
